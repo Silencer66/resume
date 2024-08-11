5dae5d48068105da7329f3e3db39108aff138a34
Artem <tema.palamar4uck@yandex.ru>
2024-08-11 20:35:17 +0300

    убрал лишнее

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    output: "export",
+    images: {
+        unoptimized: true,
+        // loader: "custom",
+        // loaderFile: "./my-loader.ts",
+    },
+    reactStrictMode: true,
 };
 
 export default nextConfig;
