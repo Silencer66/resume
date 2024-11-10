ab98b1a688fac1d8f057fe71af97f15a298dd515
Artem <tema.palamar4uck@yandex.ru>
2024-11-10 19:46:36 +0300

    вернул статику как было с next-i18static

---
+++ _next/static/chunks/pages/index-4e352f4a11da3a84.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(3430)}])},3430:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return c}});var _=t(2138);function c(){(0,_.Z9)()}}},function(n){n.O(0,[888,774,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
