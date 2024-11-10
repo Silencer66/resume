18a60a7346d7e8f5d2ecdca53f93449f9a4713da
Artem <tema.palamar4uck@yandex.ru>
2024-11-10 18:24:35 +0300

    поправил файл build manifest

--- _buildManifest.js
+++ _buildManifest.js
@@ -1,13 +1,13 @@
 (self.__BUILD_MANIFEST = {
     __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
     "/": [
-        "static/chunks/68c0a17d-0d0827f0c0583edb.js",
-        "static/chunks/cb355538-54a02baed83ebc13.js",
-        "static/chunks/41155975-e0b4dda4166ff9e1.js",
-        "static/chunks/618-4e4792a415b89ea1.js",
-        "static/chunks/pages/index-853fd62ab246d27c.js",
+        "68c0a17d-0d0827f0c0583edb.js",
+        "cb355538-54a02baed83ebc13.js",
+        "41155975-e0b4dda4166ff9e1.js",
+        "618-4e4792a415b89ea1.js",
+        "index-853fd62ab246d27c.js",
     ],
-    "/_error": ["static/chunks/pages/_error-1be831200e60c5c0.js"],
+    "/_error": ["_error-1be831200e60c5c0.js"],
     sortedPages: ["/", "/_app", "/_error"],
 }),
     self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
